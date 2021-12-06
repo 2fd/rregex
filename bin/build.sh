a1fad9f65d796f326502ae92cd0af6227596e02f
frami <i@2fd.me>
2021-12-06 09:58:19 -0300

    chore: fix pipeline

--- bin/build.sh
+++ bin/build.sh
@@ -10,20 +10,20 @@ rm lib/module.js
 mv lib/module_bg.js lib/module.js
 
 # use a single wasm file
-sed -i '' 's/browser_bg.wasm/rregex.wasm/g' lib/browser.js
-sed -i '' 's/module_bg.wasm/rregex.wasm/g' lib/module.js
-sed -i '' 's/commonjs_bg.wasm/rregex.wasm/g' lib/commonjs.js
-sed -i '' 's/__wbindgen_placeholder__/wbg/g' lib/commonjs.js
+sed -i '.tmp' -e 's/browser_bg.wasm/rregex.wasm/g' lib/browser.js
+sed -i '.tmp' -e 's/module_bg.wasm/rregex.wasm/g' lib/module.js
+sed -i '.tmp' -e 's/commonjs_bg.wasm/rregex.wasm/g' lib/commonjs.js
+sed -i '.tmp' -e 's/__wbindgen_placeholder__/wbg/g' lib/commonjs.js
 
 # fix types
 for t in lib/*.d.ts; do
-  sed -i '' 's/find(text: string): any;/find(text: string): Match | undefined;/g' $t
-  sed -i '' 's/findAt(text: string, start: number): any;/findAt(text: string, start: number): Match | undefined;/g' $t
-  sed -i '' 's/findAll(text: string): any;/findAll(text: string): Match[];/g' $t
-  sed -i '' 's/findAll(text: string): any;/findAll(text: string): Match[];/g' $t
-  sed -i '' 's/split(text: string): any;/split(text: string): string[];/g' $t
-  sed -i '' 's/splitn(text: string, limit: number): any;/splitn(text: string, limit: number): string[];/g' $t
-  sed -i '' 's/syntax(): any;/syntax(): Hir;/g' $t
+  sed -i '.tmp' -e 's/find(text: string): any;/find(text: string): Match | undefined;/g' $t
+  sed -i '.tmp' -e 's/findAt(text: string, start: number): any;/findAt(text: string, start: number): Match | undefined;/g' $t
+  sed -i '.tmp' -e 's/findAll(text: string): any;/findAll(text: string): Match[];/g' $t
+  sed -i '.tmp' -e 's/findAll(text: string): any;/findAll(text: string): Match[];/g' $t
+  sed -i '.tmp' -e 's/split(text: string): any;/split(text: string): string[];/g' $t
+  sed -i '.tmp' -e 's/splitn(text: string, limit: number): any;/splitn(text: string, limit: number): string[];/g' $t
+  sed -i '.tmp' -e 's/syntax(): any;/syntax(): Hir;/g' $t
 done
 
 # remove unused files
