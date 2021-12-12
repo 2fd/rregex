1c1808137894d2d664def24ac6ab05cb20b800ec
frami <i@2fd.me>
2021-12-12 06:21:37 -0300

    fix: enum types

--- src/types.rs
+++ src/types.rs
@@ -43,56 +43,56 @@ export type HirKindLiteralVariant = {
   '@type': 'struct'
   '@name': 'regex_syntax::hir::HirKind'
   '@variant': 'Literal'
-  'value': Literal
+  '@values': [Literal]
 }
 
 export type HirKindClassVariant = {
   '@type': 'struct'
   '@name': 'regex_syntax::hir::HirKind'
   '@variant': 'Class'
-  'value': Class
+  '@values': [Class]
 }
 
 export type HirKindAnchorVariant = {
   '@type': 'struct'
   '@name': 'regex_syntax::hir::HirKind'
   '@variant': 'Anchor'
-  'value': Anchor
+  '@values': [Anchor]
 }
 
 export type HirKindWordBoundaryVariant = {
   '@type': 'struct'
   '@name': 'regex_syntax::hir::HirKind'
   '@variant': 'WordBoundary'
-  'value': WordBoundary
+  '@values': [WordBoundary]
 }
 
 export type HirKindRepetitionVariant = {
   '@type': 'struct'
   '@name': 'regex_syntax::hir::HirKind'
   '@variant': 'Repetition'
-  'value': Repetition
+  '@values': [Repetition]
 }
 
 export type HirKindGroupVariant = {
   '@type': 'struct'
   '@name': 'regex_syntax::hir::HirKind'
   '@variant': 'Group'
-  'value': Group
+  '@values': [Group]
 }
 
 export type HirKindConcatVariant = {
   '@type': 'struct'
   '@name': 'regex_syntax::hir::HirKind'
   '@variant': 'Concat'
-  'value': Hir[]
+  '@values': [Hir[]]
 }
 
 export type HirKindAlternationVariant = {
   '@type': 'struct'
   '@name': 'regex_syntax::hir::HirKind'
   '@variant': 'Alternation'
-  'value': Hir[]
+  '@values': [Hir[]]
 }
 "#;
 
@@ -101,14 +101,14 @@ impl ToJs for hir::HirKind {
     let current = JsObject!("@type" => "enum", "@name" => "regex_syntax::hir::HirKind");
     match self {
       hir::HirKind::Empty => set!(&current, "@variant" => "Empty"),
-      hir::HirKind::Literal(l) => set!(&current, "@variant" => "Literal", "value" => l.to_js()),
-      hir::HirKind::Class(c) => set!(&current, "@variant" => "Class", "value" => c.to_js()),
-      hir::HirKind::Anchor(a) => set!(&current, "@variant" => "Anchor", "value" => a.to_js()),
-      hir::HirKind::WordBoundary(w) => set!(&current, "@variant" => "WordBoundary", "value" => w.to_js()),
-      hir::HirKind::Repetition(r) => set!(&current, "@variant" => "Repetition", "value" => r.to_js()),
-      hir::HirKind::Group(g) => set!(&current, "@variant" => "Group", "value" => g.to_js()),
-      hir::HirKind::Concat(c) => set!(&current, "@variant" => "Concat", "value" => c.to_js()),
-      hir::HirKind::Alternation(c) => set!(&current, "@variant" => "Alternation", "value" => c.to_js()),
+      hir::HirKind::Literal(l) => set!(&current, "@variant" => "Literal", "@values" => JsArray!(l.to_js())),
+      hir::HirKind::Class(c) => set!(&current, "@variant" => "Class", "@values" => JsArray!(c.to_js())),
+      hir::HirKind::Anchor(a) => set!(&current, "@variant" => "Anchor", "@values" => JsArray!(a.to_js())),
+      hir::HirKind::WordBoundary(w) => set!(&current, "@variant" => "WordBoundary", "@values" => JsArray!(w.to_js())),
+      hir::HirKind::Repetition(r) => set!(&current, "@variant" => "Repetition", "@values" => JsArray!(r.to_js())),
+      hir::HirKind::Group(g) => set!(&current, "@variant" => "Group", "@values" => JsArray!(g.to_js())),
+      hir::HirKind::Concat(c) => set!(&current, "@variant" => "Concat", "@values" => JsArray!(c.to_js())),
+      hir::HirKind::Alternation(c) => set!(&current, "@variant" => "Alternation", "@values" => JsArray!(c.to_js())),
     };
     current
   }
@@ -124,14 +124,14 @@ export type LiteralUnicodeVariant = {
   '@type': 'struct'
   '@name': 'regex_syntax::hir::Literal'
   '@variant': 'Unicode'
-  'value': string
+  '@values': [string]
 }
 
 export type LiteralByteVariant = {
   '@type': 'struct'
   '@name': 'regex_syntax::hir::Literal'
   '@variant': 'Byte'
-  'value': number
+  '@values': [number]
 }
 "#;
 
@@ -139,8 +139,8 @@ impl ToJs for hir::Literal {
   fn to_js(&self) -> JsValue {
     let current = JsObject!("@type" => "enum", "@name" => "regex_syntax::hir::Literal");
     match self {
-      hir::Literal::Unicode(unicode) => set!(&current, "@variant" => "Unicode", "value" => unicode.to_string()),
-      hir::Literal::Byte(byte) => set!(&current,  "@variant" => "Byte", "value" => byte.to_owned() as i32),
+      hir::Literal::Unicode(unicode) => set!(&current, "@variant" => "Unicode", "@values" => JsArray!(unicode.to_string())),
+      hir::Literal::Byte(byte) => set!(&current,  "@variant" => "Byte", "@values" => JsArray!(byte.to_owned() as i32)),
     };
     current
   }
@@ -156,14 +156,14 @@ export type ClassUnicodeVariant = {
   '@type': 'struct'
   '@name': 'regex_syntax::hir::Class'
   '@variant': 'Unicode'
-  'value': ClassUnicode
+  '@values': [ClassUnicode]
 }
 
 export type ClassByteVariant = {
   '@type': 'struct'
   '@name': 'regex_syntax::hir::Class'
   '@variant': 'Bytes'
-  'value': ClassBytes
+  '@values': [ClassBytes]
 }
 "#;
 
@@ -171,8 +171,8 @@ impl ToJs for hir::Class {
   fn to_js(&self) -> JsValue {
     let current = JsObject!("@type" => "enum", "@name" => "regex_syntax::hir::Class");
     match self {
-      hir::Class::Unicode(unicode) => set!(&current, "@variant" => "Unicode", "value" => unicode.to_js()),
-      hir::Class::Bytes(byte) => set!(&current, "@variant" => "Bytes", "value" => byte.to_js()),
+      hir::Class::Unicode(unicode) => set!(&current, "@variant" => "Unicode", "@values" => JsArray!(unicode.to_js())),
+      hir::Class::Bytes(byte) => set!(&current, "@variant" => "Bytes", "@values" => JsArray!(byte.to_js())),
     };
     current
   }
@@ -401,7 +401,7 @@ export type RepetitionKindRangeVariant = {
   '@type': 'struct'
   '@name': 'regex_syntax::hir::Repetition'
   '@variant': 'Range'
-  value: RepetitionRange
+  '@values': [RepetitionRange]
 }
 "#;
 
@@ -412,7 +412,7 @@ impl ToJs for hir::RepetitionKind {
       hir::RepetitionKind::ZeroOrOne => set!(&current, "@variant" => "ZeroOrOne"),
       hir::RepetitionKind::ZeroOrMore => set!(&current, "@variant" => "ZeroOrMore"),
       hir::RepetitionKind::OneOrMore => set!(&current, "@variant" => "OneOrMore"),
-      hir::RepetitionKind::Range(range) => set!(&current, "@variant" => "Range", "value" => range.to_js()),
+      hir::RepetitionKind::Range(range) => set!(&current, "@variant" => "Range", "@values" => JsArray!(range.to_js())),
     };
     current
   }
@@ -429,21 +429,21 @@ export type RepetitionRangeExactlyVariant = {
   '@type': 'struct'
   '@name': 'regex_syntax::hir::RepetitionRange'
   '@variant': 'Exactly'
-  value: number
+  '@values': [number]
 }
 
 export type RepetitionRangeAtLeastVariant = {
   '@type': 'struct'
   '@name': 'regex_syntax::hir::RepetitionRange'
   '@variant': 'AtLeast'
-  value: number
+  '@values': [number]
 }
 
 export type RepetitionRangeBoundedVariant = {
   '@type': 'struct'
   '@name': 'regex_syntax::hir::RepetitionRange'
   '@variant': 'Bounded'
-  value: [number, number]
+  '@values': [number, number]
 }
 "#;
 
@@ -451,9 +451,9 @@ impl ToJs for hir::RepetitionRange {
   fn to_js(&self) -> JsValue {
     let current = JsObject!("@type" => "enum", "@name" => "regex_syntax::hir::RepetitionRange");
     match self {
-      hir::RepetitionRange::Exactly(val) => set!(&current, "@variant" => "Exactly", "value" => val.to_owned() as f64),
-      hir::RepetitionRange::AtLeast(min) => set!(&current, "@variant" => "AtLeast", "value" => min.to_owned() as f64),
-      hir::RepetitionRange::Bounded(min, max) => set!(&current, "@variant" => "Bounded", "value" => JsArray!(min.to_owned() as f64, max.to_owned() as f64)),
+      hir::RepetitionRange::Exactly(val) => set!(&current, "@variant" => "Exactly", "@values" => JsArray!(val.to_owned() as f64)),
+      hir::RepetitionRange::AtLeast(min) => set!(&current, "@variant" => "AtLeast", "@values" => JsArray!(min.to_owned() as f64)),
+      hir::RepetitionRange::Bounded(min, max) => set!(&current, "@variant" => "Bounded", "@values" => JsArray!(min.to_owned() as f64, max.to_owned() as f64)),
     };
     current
   }
