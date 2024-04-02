9bc1a472ec063e7d0b7c96c8b24c0892e7276f03
frami <i@2fd.me>
2024-04-02 17:28:14 -0300

    chore: improve install script

--- bin/install.sh
+++ bin/install.sh
@@ -3,20 +3,22 @@ CARGO_VERSION=$(cargo version)
 
 ## Install rust if is missing
 if [ "$CARGO_VERSION" = "" ]; then
-  echo "    ➡️ installing rust"
+  echo "    📥 installing rust"
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-else
-  echo "    ✅ $CARGO_VERSION"
+  CARGO_VERSION=$(cargo version)
 fi
+
+echo "    ✅ $CARGO_VERSION ($(which cargo))"
+echo ""
 
 WASM_PACK_VERSION=$(wasm-pack --version)
 
 ## Install wasm-pack if is missing
 if [ "$WASM_PACK_VERSION" = "" ]; then
-  echo "    ➡️ installing wasm-pack"
+  echo "    📥 installing wasm-pack"
   cargo install wasm-pack
+  WASM_PACK_VERSION=$(wasm-pack --version)
 else
-  echo "    ✅ $WASM_PACK_VERSION"
-fi
 
+echo "    ✅ $WASM_PACK_VERSION ($(which wasm-pack))"
 echo ""
